8b2c95b485e59e1d9fb61e64275ba8b826599ddb
bbaekdo <05163@naver.com>
2016-03-29 23:36:21 +0900

    w4_hw

---
+++ src/com/j2/w4/TurtleKeyObserverMain.java
@@ -0,0 +1,44 @@
+package com.j2.w4;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import ch.aplu.turtle.*;
+
+class TurtleKeyListener extends Turtle {
+  public TurtleKeyListener() {
+    addKeyListener(new KeyAdapter() {
+    public void keyPressed(KeyEvent e) {
+      int keyCode= e.getKeyCode();
+      switch(keyCode) {
+        case KeyEvent.VK_UP:
+          setStatusText("forward 20");
+          forward(20);
+          break;
+        case KeyEvent.VK_DOWN:
+          setStatusText("back 20");
+          back(20);
+          break;
+        case KeyEvent.VK_LEFT:
+          setStatusText("turn left");
+          left(90);
+          break;
+        case KeyEvent.VK_RIGHT:
+          setStatusText("turn right");
+          right(90);
+          break;
+        case KeyEvent.VK_Q:
+          setStatusText("q pressed and exiting");
+          break;
+    }
+    }
+    });
+  }
+}
+
+public class TurtleKeyObserverMain {
+  public static void main(String[] args) {
+    TurtleKeyListener t=new TurtleKeyListener();
+    t.addStatusBar(20);
+    t.setStatusText("Press any key!");
+  }
+}
