90e1e78a846849439e01f375707d0381b864d316
pce913 <pce0913@naver.com>
2016-04-11 18:19:26 +0900

    w6

---
+++ src/com/j2/w6/NYPizzaStore.java
@@ -0,0 +1,14 @@
+package com.j2.w6;
+
+public class NYPizzaStore extends PizzaStore {
+  Pizza createPizza(String type) {
+    if (type.equals("cheese")) {
+      return new NYStyleCheesePizza();
+    } else if (type.equals("veggie")) {
+      return new NYStyleVeggiePizza();
+    } else return null;
+  }
+}
+
+
+  
---
+++ src/com/j2/w6/PizzaStore.java
@@ -0,0 +1,25 @@
+package com.j2.w6;
+
+/*
+public abstract class PizzaStore {
+  SimplePizzaFactory factory;
+  public PizzaStore(SimplePizzaFactory factory) {
+    this.factory=factory;
+  }
+  public Pizza orderPizza(String type) {
+    Pizza pizza=factory.createPizza(type);
+    pizza.prepare();
+  }
+}
+*/
+
+public abstract class PizzaStore {
+  abstract Pizza createPizza(String type);
+  public Pizza orderPizza(String type) {
+    Pizza pizza=createPizza(type);
+    pizza.prepare();
+    pizza.bake();
+    pizza.cut();
+    return pizza;
+  }
+}
